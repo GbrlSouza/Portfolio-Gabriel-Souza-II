7cfac9f5da90e847acd1515a526c6bc67098479f
Gabriel Costa de Souza <gabrielsouza_1909@hotmail.com>
2024-09-26 17:10:46 -0300

    Create ContactSection.vue

---
+++ ContactSection.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container id="contact">
+    <h2>Contato</h2>
+    <v-form>
+      <v-text-field label="Nome" required></v-text-field>
+      <v-text-field label="E-mail" required></v-text-field>
+      <v-textarea label="Mensagem" required></v-textarea>
+      <v-btn color="blue darken-3" dark>Enviar</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ContactSection',
+}
+</script>
+
+<style scoped>
+h2 {
+  color: #2196f3;
+}
+</style>
