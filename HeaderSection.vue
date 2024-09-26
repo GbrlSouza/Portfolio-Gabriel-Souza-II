b6cc3ec5f4ed744960b4b2728fbbc0dc89a54bce
Gabriel Costa de Souza <gabrielsouza_1909@hotmail.com>
2024-09-26 17:07:52 -0300

    Create HeaderSection.vue

---
+++ HeaderSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-toolbar flat dark color="blue darken-3">
+    <v-toolbar-title>João Silva | Full Stack Developer Jr</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn text to="#about">Sobre</v-btn>
+    <v-btn text to="#skills">Skills</v-btn>
+    <v-btn text to="#projects">Projetos</v-btn>
+    <v-btn text to="#contact">Contato</v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'HeaderSection',
+}
+</script>
+
+<style scoped>
+.v-toolbar-title {
+  font-weight: bold;
+}
+</style>
